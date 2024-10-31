c4f2fe2a6e34e1b4ce7ec0d2951b7f1db1498398
zzll37 <2548466117@qq.com>
2024-10-31 23:22:59 +0800

    修改了运动界面，调整手变

--- app/src/main/java/com/example/autobike/Shoubian.java
+++ app/src/main/java/com/example/autobike/Shoubian.java
@@ -1,8 +1,12 @@
 package com.example.autobike;
 
+import static com.example.autobike.bluetooth.BleCallback.crc;
+
+import android.Manifest;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +15,7 @@ import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -21,12 +26,16 @@ import com.example.map.R;
 import com.example.autobike.utils.BleHelper;
 import com.example.autobike.bluetooth.BleCallback;
 
+import com.example.autobike.utils.BleHelper;
+import com.example.autobike.bluetooth.BleCallback;
+
 public class Shoubian extends AppCompatActivity {
 
-    private Button swapButton,button1,button2,button3;
-    private ImageView icon1,icon2;
+    private Button swapButton, button1, button2, button3;
+    private ImageView icon1, icon2;
 
     private BluetoothGatt bluetoothGatt;
+    private BluetoothGatt gatt;
     private boolean isConnected = false;
     private BleCallback bleCallback;
     private EditText etCommand;
@@ -42,11 +51,27 @@ public class Shoubian extends AppCompatActivity {
             return insets;
         });
 
+        //读左手变当前键位值
+        BleHelper.sendCommand(gatt, "28030000"+crc,true);
+
+        //读右手变当前键位值
+        BleHelper.sendCommand(gatt, "29030000"+crc,true);
+
         //初始化
         bleCallback = new BleCallback();
         //获取上个页面传递过来的设备
         BluetoothDevice device = getIntent().getParcelableExtra("device");
         //连接gatt 设置Gatt回调
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
         bluetoothGatt = device.connectGatt(this, false, bleCallback);
 
         swapButton=findViewById(R.id.swapBUtton);
@@ -90,6 +115,10 @@ public class Shoubian extends AppCompatActivity {
                 BleHelper.sendCommand(bluetoothGatt, "2A0600006985",true);
                 icon1.layout(icon2Left,icon2Top,icon2Left+icon1.getWidth(),icon2Top+icon1.getWidth());
                 icon2.layout(icon1Left,icon1Top,icon1Left+icon2.getWidth(),icon1Top+icon2.getWidth());
+
+                //左右手变控制互换（写）
+                BleHelper.sendCommand(gatt, "2A060100"+crc,true);
+
             }
         });
     }
